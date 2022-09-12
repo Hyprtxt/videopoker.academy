4d84b647268258ce0512bb262f32842883c1bc72
Taylor Y <tdy721@gmail.com>
2022-09-11 21:24:19 -0700

    debug island for DENO_ENV === development only

--- islands/Debug.jsx
+++ islands/Debug.jsx
@@ -1,24 +1,35 @@
-export const Debug = ({ data }) =>
-  data
+import { useState } from "preact/hooks";
+
+export const Debug = ({ data }) => {
+  const [open, setOpen] = useState(false);
+  return data
     ? (
-      <>
-        <p>
+      <div class="p-4 mx-auto max-w-screen-md">
+        <p class="mb-4">
           <button
-            class="btn btn-dark"
+            class="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#collapseExample"
+            onClick={(_e) => {
+              setOpen((current) => !current);
+            }}
           >
-            Show View Props
+            {open ? `Hide` : `Show`} View Props
           </button>
         </p>
-        <div class="collapse" id="collapseExample">
-          <div class="card card-body">
+        <div class={open ? `` : `hidden`} id="collapseExample">
+          <div class="bg-white border rounded p-3">
             <pre>{JSON.stringify(data, null, 2)}</pre>
           </div>
         </div>
-      </>
+      </div>
     )
-    : <p>No data found</p>;
+    : (
+      <div class="p-4 mx-auto max-w-screen-md">
+        <p class="mb-4">No developer data found</p>
+      </div>
+    );
+};
 
 export default Debug;
