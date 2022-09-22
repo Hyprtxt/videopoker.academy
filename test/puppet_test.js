65d5366d40b9101f833da594f2a15d204b612c68
Taylor Young <taylor@Taylors-MacBook-Pro.local>
2022-09-22 11:56:35 -0700

    test

--- test/puppet_test.js
+++ test/puppet_test.js
@@ -18,17 +18,29 @@ Deno.test(
       });
       assertEquals(response.status(), 200);
     });
+
     await t.step("The draw page should 404 a GET", async () => {
       const response = await page.goto(`${BASE_URL}/draw`, {
         waitUntil: "networkidle2",
       });
       assertEquals(response.status(), 404);
     });
+
     await t.step("The player page should work", async () => {
       const response = await page.goto(`${BASE_URL}/player`, {
         waitUntil: "networkidle2",
       });
       assertEquals(response.status(), 200);
+    });
+
+    await t.step("The deal page should allow gameplay", async () => {
+      const response = await page.goto(`${BASE_URL}/deal`, {
+        waitUntil: "networkidle2",
+      });
+      assertEquals(response.status(), 200);
+      await page.click('[type="submit"]');
+      await page.waitForNetworkIdle();
+      assertEquals(page.url(), `${BASE_URL}/draw`);
     });
   }),
 );
