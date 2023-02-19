0094f66456fbd8956ad7faaf12115bd4a13c48e0
Taylor Y <tdy721@gmail.com>
2023-02-19 13:46:45 -0700

    fix

--- routes/index.jsx
+++ routes/index.jsx
@@ -215,7 +215,7 @@ export default function Home({ data }) {
         </p>
         <a
           href="//classic.videopoker.academy"
-          class="bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded"
+          class={LINK_CLASS}
         >
           Classic Video Poker
         </a>
