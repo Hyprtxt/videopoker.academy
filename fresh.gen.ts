470906cdaaa10db85fb0159a3ef2f44858104125
Taylor Y <tdy721@gmail.com>
2023-03-06 09:55:31 -0700

    fresh_seo

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,11 +5,11 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_404.jsx";
 import * as $1 from "./routes/_middleware.jsx";
-import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/deal.jsx";
-import * as $4 from "./routes/draw.jsx";
-import * as $5 from "./routes/index.jsx";
-import * as $6 from "./routes/player.jsx";
+import * as $2 from "./routes/deal.jsx";
+import * as $3 from "./routes/draw.jsx";
+import * as $4 from "./routes/index.jsx";
+import * as $5 from "./routes/player.jsx";
+import * as $6 from "./routes/sitemap.xml.js";
 import * as $$0 from "./islands/Card.jsx";
 import * as $$1 from "./islands/Debug.jsx";
 import * as $$2 from "./islands/Keyboard.jsx";
@@ -19,11 +19,11 @@ const manifest = {
   routes: {
     "./routes/_404.jsx": $0,
     "./routes/_middleware.jsx": $1,
-    "./routes/api/joke.ts": $2,
-    "./routes/deal.jsx": $3,
-    "./routes/draw.jsx": $4,
-    "./routes/index.jsx": $5,
-    "./routes/player.jsx": $6,
+    "./routes/deal.jsx": $2,
+    "./routes/draw.jsx": $3,
+    "./routes/index.jsx": $4,
+    "./routes/player.jsx": $5,
+    "./routes/sitemap.xml.js": $6,
   },
   islands: {
     "./islands/Card.jsx": $$0,
